4207044fd7831b7919275d493faf1a2aa29b632c
Amirhossein <ahm1371ahm@gmail.com>
2023-05-03 16:51:02 +0330

    adding compiler macros

--- test/main.cpp
+++ test/main.cpp
@@ -4,6 +4,14 @@
 
 int main(int arc, char *argv[])
 {
+#if __clang__
+    std::cout << "compiled with clang++" << std::endl;
+#elif __GNUC__
+    std::cout << "complied with GNU" << std::endl;
+#elif ___MSC_VER
+    std::cout << "compiled with MSVC" << std::endl;
+#endif
+
     NVCounter nvCounter("counter2.txt");
     ++nvCounter;
     nvCounter -= 34;
